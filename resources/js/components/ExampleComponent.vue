603bea117f8718b8b7fac8886553c89984f49a58
Rizky Ibrahim <rizky.ibrahim@opencloud.id>
2022-06-19 01:19:19 +0700

    Install Laravel 9.x, Vue 3.x dan dependencies

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>{{ title }}</h1>
+
+  <p>This is an example component.</p>
+</template>
+<script>
+export default {
+  setup: () => ({
+    title: 'How To Install Vue 3 in Laravel 8 From Scratch'
+  })
+}
+</script>
